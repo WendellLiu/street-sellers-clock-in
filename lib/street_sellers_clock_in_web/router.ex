21b3ce319ae4792ba749bef54f69a61c4af54947
WendellLiu <cuk.bas@gmail.com>
2018-07-09 14:23:40 +0800

    router

--- lib/street_sellers_clock_in_web/router.ex
+++ lib/street_sellers_clock_in_web/router.ex
@@ -5,12 +5,22 @@ defmodule StreetSellersClockInWeb.Router do
     plug :accepts, ["json"]
   end
 
-  scope "/api", StreetSellersClockInWeb do    
+  scope "/api", StreetSellersClockInWeb do
     pipe_through :api
 
     resources "/users", UserController, except: [:create]
-    resources "/product/categories", CategoryController
-    resources "/clock_in/clock_record", ClockRecordController
+  end
+
+  scope "/api/product", StreetSellersClockInWeb do
+    pipe_through :api
+
+    resources "/categories", CategoryController
+  end
+
+  scope "/api/clock_in", StreetSellersClockInWeb do
+    pipe_through :api
+
+    resources "/clock_record", ClockRecordController
   end
 
   scope "/api/auth", StreetSellersClockInWeb do
