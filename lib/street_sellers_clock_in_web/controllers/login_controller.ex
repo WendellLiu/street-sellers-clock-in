588516e5a9a9963f9c9fe0dea432f6d2386cb630
WendellLiu <cuk.bas@gmail.com>
2018-07-26 18:01:06 +0800

    /login

--- lib/street_sellers_clock_in_web/controllers/login_controller.ex
+++ lib/street_sellers_clock_in_web/controllers/login_controller.ex
@@ -1,18 +1,32 @@
 defmodule StreetSellersClockInWeb.LoginController do
   use StreetSellersClockInWeb, :controller
 
+  import Ecto.Changeset
   alias StreetSellersClockIn.Accounts
   alias StreetSellersClockIn.Accounts.LoginToken
+  alias Utils.Auth.Password
+  alias Utils.Auth.LoginToken
 
   action_fallback StreetSellersClockInWeb.FallbackController
 
   def create(conn, %{"login" => login_params}) do
-    with {:ok, %LoginToken{} = login} <- Accounts.create_login(login_params) do
-      conn
-      |> put_status(:created)
-      |> put_resp_header("location", login_path(conn, :show, login))
-      |> render("show.json", login: login)
+    %{
+      "username"=> username,
+      "password"=> password,
+    } = login_params
+
+    with user when not is_nil(user) <- Accounts.get_user_by_attr!(%{username: username}) do
+      # FIXME: bug
+      hash = get_field(user, :password)
+      IO.puts(hash)
+
     end
+    # with {:ok, %LoginToken{} = login} <- Accounts.create_login(login_params) do
+    #   conn
+    #   |> put_status(:created)
+    #   |> put_resp_header("location", login_path(conn, :show, login))
+    #   |> render("show.json", login: login)
+    # end
   end
 
   # def show(conn, %{"id" => id}) do
